88382de56c50584d9223f4d1bf93377c86045010
symartin <31699829+symartin@users.noreply.github.com>
2017-09-21 18:31:40 +0200

    Premiere tentative pour lire les frame MDA

--- MDTfile.py
+++ MDTfile.py
@@ -1,3 +1,4 @@
+import binascii
 import io
 import os
 from struct import *
@@ -17,14 +18,29 @@ class MDTFile:
         def read_uint16(self):
             return unpack("<H", self._file.read(2))[0]
 
+        def read_uint64(self):
+            return unpack("<Q", self._file.read(8))[0]
+
         def read_uint32(self):
             return unpack("<I", self._file.read(4))[0]
+
+        def read_int32(self):
+            return unpack("<i", self._file.read(4))[0]
 
         def read_char(self):
             return unpack("<c", self._file.read(1))[0]
 
         def read_uchar(self):
             return int(unpack('<B', self._file.read(1))[0])
+
+        def read_double(self):
+            return float(unpack('<d', self._file.read(8))[0])
+
+        def read_float64(self):
+                return self.read_double()
+
+        def read_float32(self):
+            return float(unpack('<f', self._file.read(4))[0])
 
         def __getattr__(self, attr):
             return getattr(self._file, attr)
@@ -47,7 +63,7 @@ class MDTFile:
             for i in range(mdt_file.last_frame + 1):
                 frame = MDTFrame()
                 frame.read_header(self._file)
-                frame.print_header()
+                #frame.print_header()
 
 
                 if frame.type == MDTFrameType.MDT_FRAME_SCANNED:
@@ -65,6 +81,7 @@ class MDTFile:
                     pass
 
                 elif frame.type == MDTFrameType.MDT_FRAME_MDA:
+                    frame.read_mda_frame(self._file)
                     pass
 
                 elif frame.type == MDTFrameType.MDT_FRAME_CURVES_NEW:
@@ -134,6 +151,32 @@ class MDTFrame:
      self.data       = None
      self.metadata   = ""
      self.title      = ""
+
+
+
+     self.array_size    = 0
+     self.cell_size     = 0
+     self.nb_dimensions = 0
+     self.dimensions    = []
+     self.nb_mesurands  = 0
+     self.mesurands     = []
+
+     self.xy_unit       = ""
+
+    def unit_code_for_si_code(self,code):
+        """transform the binary code for unit in the mda frame to understandable unit """
+        if int(code) == 0x0000000000000001:
+            return MDTUnit.MDT_UNIT_NONE
+        elif int(code) == 0x0000000000000101:
+            return MDTUnit.MDT_UNIT_METER
+        elif int(code) == 0x0000000000100001:
+            return MDTUnit.MDT_UNIT_VOLT2
+        elif int(code) == 0x000000fffd010200:
+            return MDTUnit.MDT_UNIT_SECOND
+        elif int(code) == 0x0000000001000001:
+            return MDTUnit.MDT_UNIT_NONE
+        else :
+            return MDTUnit.MDT_UNIT_SECOND
 
     def read_header(self, file):
         """
@@ -245,8 +288,227 @@ class MDTFrame:
         # the characters are packed on 2 bytes (it's UTF-16)
         self.metadata = file.read(xml_len).decode('utf-16')
 
+    def mdt_read_mda_calibration(self,file):
+
+        calibration = dict()
+
+        starting_position = file.tell()
+
+        calibration['total_len']= file.read_uint32()
+
+        struct_len = file.read_uint32()
+        sp = file.tell() + struct_len
+
+        calibration['name_len']     = file.read_uint32()
+        calibration['comment_len']  = file.read_uint32()
+        calibration['unit_len']     = file.read_uint32()
+        calibration['unit_code']    = file.read_uint64() #si_unit
+        calibration['accuracy']     = file.read_double()
+        calibration['fct_id']       = file.read_uint32()
+        calibration['fct_pointer']  = file.read_uint32()
+        #file.shift_stream_position(8) #skip function id and dimensions
+        calibration['bias']         = file.read_double()
+        calibration['scale']        = file.read_double()
+        calibration['min_index']    = file.read_uint64()
+        calibration['max_index']    = file.read_uint64()
+        calibration['data_type']    = file.read_int32() #not an unsigned int !
+        calibration['author_len']   = file.read_uint32()
+
+        fint_entete = file.tell()
+        file.shift_stream_position(36)
+
+        def extract_string(string_len):
+            if string_len>0 :
+                if string_len==1:
+                    return file.read_uchar()
+                else:
+                    return file.read(string_len)
+            else :
+                return None
+
+        calibration['name'] = extract_string(calibration['name_len'])
+        file.seek(sp) # apparently there is 36 byte after the header not used (at least here)
+
+
+
+        calibration['comment'] = extract_string(calibration['comment_len'])
+        calibration['unit'] = extract_string(calibration['unit_len'])
+        calibration['author'] = extract_string(calibration['author_len'])
+        calibration['comment'] = extract_string(calibration['comment_len'])
+
+        file.seek(starting_position + calibration['total_len'])
+
+        print(calibration)
+        return calibration
+
+    def extract_mda_data(self,file):
+        x_axis = self.dimensions[0]
+        y_axis = self.dimensions[1]
+        z_axis = self.mesurands[0]
+
+
+        if x_axis['unit'] and x_axis['unit_len']:
+            try :
+                self.xy_unit = x_axis['unit'].decode('utf-8','replace')
+                #TODO implement gwy_si_unit_new_parse(unit, &power10xy);
+            except AttributeError as e:
+                print(e)
+        else:
+            pass
+            # TODO implement here !
+            #cunit = gwy_flat_enum_to_string(unitCodeForSiCode(xAxis->siUnit),
+            #G_N_ELEMENTS(mdt_units),
+            #mdt_units, mdt_units_name);
+            #siunitxy = gwy_si_unit_new_parse(cunit, & power10xy);
+
+        nx  = x_axis['max_index'] - x_axis['min_index'] + 1
+        ny = y_axis['max_index'] - y_axis['min_index'] + 1
+        xreal = x_axis['scale'] * (nx - 1)
+        yreal = y_axis['scale'] * (ny - 1)
+        zscale = z_axis['scale']
+        zoffset =  z_axis['bias']
+
+        total = nx * ny
+        result = []
+        if  z_axis['data_type']== MDADataType.MDA_DATA_INT8:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_UINT8:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_INT16:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_UINT16:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_INT32:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_UINT32:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_INT64:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_UINT64:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_FLOAT32:
+            pass
+        if  z_axis['data_type']== MDADataType.MDA_DATA_FLOAT64:
+            for i in range(total):
+                result.append(file.read_float64())
+
+
+        else :
+            print('ERROR ') #TODO manage the error
+
+        print(result)
     def read_mda_frame(self,file):
-        pass
+
+        starting_position = file.tell()
+
+        # the header size and te total size
+        head_size = file.read_uint32() #usaly 76 bytes
+        total_len = file.read_uint32()
+
+        #skip guids and frame status
+        guids = [str(binascii.hexlify(bytearray(file.read(16)))), str(binascii.hexlify(bytearray(file.read(16))))]
+
+        # skip the 4 0x00 bytes
+        file.shift_stream_position(4)
+
+        #info block
+        title_size = file.read_uint32()
+        xml_size = file.read_uint32()
+        view_info_size = file.read_uint32()
+        spec_size = file.read_uint32()
+        source_info_size = file.read_uint32()
+        var_size = file.read_uint32()
+
+        #skip data offset
+        file.shift_stream_position(4)
+
+        #prints for debug
+        data_size = file.read_uint32()
+        # print("Header")
+        # print("------")
+        # print("head_size %u" % head_size)
+        # print("tot_len %u"% total_len)
+        # print("guids %s" % guids[0])
+        # print("name size %u" % title_size)
+        # print("xml size %u" % xml_size)
+        # print("view info size %u" % view_info_size)
+        # print("spec size %u" % spec_size)
+        # print("source info size %u" % source_info_size)
+        # print("var size %u"% var_size)
+        # print("data size %u"% data_size)
+        # print("---------------------------------------")
+
+        if total_len < head_size : raise Exception("the frame size is smaller than the header size")
+
+        # to be sure to start reading at the right place
+        file.seek(starting_position + head_size)
+
+        if title_size != 0 and (self.size - (file.tell() - self.fstart)) >= title_size :
+            self.title = file.read(title_size).decode('utf-8')
+        else :
+            self.title =""
+
+        if xml_size and (self.size - (file.tell() - self.fstart)) >= xml_size :
+            self.metadata = file.read(xml_size).decode('utf-16')
+
+        print('-----------------')
+        print("frame name: %s" % self.title)
+        # print("frame metadata: %s" % self.metadata)
+        print('-----------------')
+
+        # skip FrameSpec ViewInfo SourceInfo and vars
+        file.shift_stream_position(spec_size) # I clearly don't know what is the FrameSpec...
+        file.shift_stream_position(view_info_size)
+        file.shift_stream_position(source_info_size)
+
+        # after there is again a 4 bytes integer with the size of the data
+        if var_size != file.read_uint32() :
+            raise Exception("The variable size indicated in the header is not the same that the one put in the data")
+
+        struct_len = file.read_uint32()
+        struct_pointer = file.tell()
+
+        self.array_size = file.read_uint64()
+        self.cell_size = file.read_uint32()
+        self.nb_dimensions = file.read_uint32()
+        self.nb_mesurands = file.read_uint32()
+
+        file.seek(struct_pointer + struct_len)
+
+        if self.nb_dimensions != 0 :
+            for i in range(self.nb_dimensions) :
+                self.dimensions.append(self.mdt_read_mda_calibration(file))
+
+        if self.nb_mesurands != 0:
+            for i in range(self.nb_mesurands):
+                self.mesurands.append(self.mdt_read_mda_calibration(file))
+
+        if self.nb_dimensions == 2 and self.nb_mesurands == 1:
+            self.extract_mda_data(file)
+
+        """
+        /* scan */
+                dfield = extract_mda_data(mdaframe);
+                g_string_printf(key, "/%d/data", n);
+                gwy_container_set_object_by_name(data, key->str, dfield);
+                g_object_unref(dfield);
+                if (mdaframe->title) {
+                    g_string_append(key, "/title");
+                    gwy_container_set_string_by_name(data, key->str,
+                                            g_strdup_printf("%s (%u)",
+                                            mdaframe->title, i+1));
+                    g_free((gpointer)mdaframe->title);
+                }
+                else
+                    gwy_app_channel_title_fall_back(data, n);
+
+                meta = mdt_get_metadata(&mdtfile, i);
+                g_string_printf(key, "/%d/meta", n);
+                gwy_container_set_object_by_name(data, key->str, meta);
+                g_object_unref(meta);
+                gwy_file_channel_import_log_add(data, n, NULL, filename);
+
+        """
 
     def print_header(self):
         """
